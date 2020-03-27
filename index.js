9db50dca5b396266a5ea17cee6f2c7417fa94e10
kimtuyentran <59691394+kimtuyentran@users.noreply.github.com>
2020-03-27 13:53:25 +0900

    a

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ hi: 'hello' });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+//https://boiling-anchorage-41931.herokuapp.com/ | https://git.heroku.com/boiling-anchorage-41931.git
